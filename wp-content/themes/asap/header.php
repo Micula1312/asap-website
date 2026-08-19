f6ff5f3ac08ee8c8d03830f6e3d5e68742c69625
Micula31 <tecnoboskiva1312@proton.me>
2026-08-19 18:36:42 +0200

    Right align larger navigation and anchor About on homepage

--- wp-content/themes/asap/header.php
+++ wp-content/themes/asap/header.php
@@ -11,35 +11,18 @@
 <div class="rainbow-bar" aria-hidden="true"></div>
 
 <header class="site-header shell">
-    <a class="site-brand" href="<?php echo esc_url(home_url('/')); ?>" aria-label="ASAP home">
-        ASAP
-    </a>
+    <a class="site-brand" href="<?php echo esc_url(home_url('/')); ?>" aria-label="ASAP home">ASAP</a>
 
     <nav class="site-nav" aria-label="Primary">
-        <?php
-        if (has_nav_menu('primary')) {
-            wp_nav_menu([
-                'theme_location' => 'primary',
-                'container' => false,
-                'menu_class' => 'menu-list',
-                'fallback_cb' => false,
-            ]);
-        } else {
-            ?>
-            <div class="menu-list">
-                <a class="pill" href="<?php echo esc_url(home_url('/about')); ?>">About</a>
-                <a class="pill" href="<?php echo esc_url(home_url('/works')); ?>">Works</a>
-                <a class="pill" href="<?php echo esc_url(home_url('/ex-casa-del-custode')); ?>">Ex Casa del Custode</a>
-                <a class="pill" href="<?php echo esc_url(home_url('/radio')); ?>">Radio</a>
-            </div>
-            <?php
-        }
-        ?>
+        <div class="menu-list">
+            <a class="menu-link" href="<?php echo esc_url(is_front_page() ? '#about' : home_url('/#about')); ?>">About</a>
+            <a class="menu-link" href="<?php echo esc_url(get_post_type_archive_link('work')); ?>">Works</a>
+            <a class="menu-link" href="<?php echo esc_url(home_url('/ex-casa-del-custode')); ?>">Ex Casa del Custode</a>
+            <a class="menu-link menu-link--radio" href="<?php echo esc_url(home_url('/radio')); ?>">Radio</a>
+        </div>
     </nav>
 
-    <button class="pill calendar-trigger" type="button" aria-haspopup="dialog" aria-controls="calendar-overlay" aria-expanded="false">
-        Calendar
-    </button>
+    <button class="pill calendar-trigger" type="button" aria-haspopup="dialog" aria-controls="calendar-overlay" aria-expanded="false">Calendar</button>
 </header>
 
 <div class="calendar-overlay" id="calendar-overlay" role="dialog" aria-modal="true" aria-hidden="true" aria-label="ASAP calendar">
