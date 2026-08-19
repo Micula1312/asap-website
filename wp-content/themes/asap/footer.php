8c9e58b2cb75e1add145d6fd236bd027eb96ba1b
Micula31 <tecnoboskiva1312@proton.me>
2026-08-19 18:24:54 +0200

    Overlay homepage footer like portfolio

--- wp-content/themes/asap/footer.php
+++ wp-content/themes/asap/footer.php
@@ -1,9 +1,8 @@
 <footer class="site-footer shell">
     <div>ASAP APS</div>
-    <div class="site-footer__links">
-        <a href="https://www.instagram.com/a_s_a_p/" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="mailto:info@asapaps.it">info@asapaps.it</a>
-    </div>
+    <a href="<?php echo esc_url(home_url('/')); ?>">www.asapaps.it</a>
+    <a href="https://www.instagram.com/a_s_a_p/" target="_blank" rel="noopener noreferrer">Instagram @a_s_a_p</a>
+    <a href="mailto:info@asapaps.it">info@asapaps.it</a>
 </footer>
 
 <?php wp_footer(); ?>
