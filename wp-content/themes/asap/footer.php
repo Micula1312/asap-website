053003dd16822b719375b5a128cf43d093949ed2
Micula31 <tecnoboskiva1312@proton.me>
2026-08-19 16:54:45 +0200

    Add ASAP footer

---
+++ wp-content/themes/asap/footer.php
@@ -0,0 +1,11 @@
+<footer class="site-footer shell">
+    <div>ASAP APS</div>
+    <div class="site-footer__links">
+        <a href="https://www.instagram.com/a_s_a_p/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="mailto:info@asapaps.it">info@asapaps.it</a>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
